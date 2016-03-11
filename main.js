3109ee0c9f0b6f754b6381e2560720fb5bd3df72
pattyboyo <patrick2hb@hotmail.com>
2016-03-10 21:07:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
    
  
   $("button").click(function() {
-     $(".main, .topbar").toggle("explode");
+     $(".main, .topbar, button").toggle("explode");
      
   }
      
