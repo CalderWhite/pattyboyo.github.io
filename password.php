3b792b02e9e9ac3f7159f72acaf4bbedbbb8c6f1
pattyboyo <patrick2hb@hotmail.com>
2016-03-12 21:59:30 -0500

    Update password.php

--- password.php
+++ password.php
@@ -1,1 +1,16 @@
+<html>
+<body>
 
+ <?php if ($_POST["password"] === 'yourmotherwasamurloc') {
+    echo 'wow, you wasted ur time with inspect element';
+ } else {
+   echo "wrong, don't come again";
+   <script> windows.history.back() </script>
+ }
+   
+   
+      ?>
+
+
+</body>
+</html>
