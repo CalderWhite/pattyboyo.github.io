c79be6f31cfbcc5fa77f3ddfe394762c831792fa
pattyboyo <patrick2hb@hotmail.com>
2016-03-17 18:50:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,8 +19,10 @@ $(document).ready(function() {
      $('body').delay(8100).append("<img src='patrick.png'> <form action='www.google.ca'>  <input type='text'> <input type='submit'> </form>");
         })
   $( "form" ).submit(function( event ) {
+    event.preventDefault();
   if ( $( "input:first" ).val() === "correct" ) {
    alert("something");
+    event.preventDefault();
   }
     event.preventDefault();
  
