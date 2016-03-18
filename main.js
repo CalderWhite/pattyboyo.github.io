06e1bb562f71f20c1886dbab54fdcf9db3f6bb98
pattyboyo <patrick2hb@hotmail.com>
2016-03-18 13:28:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,8 +20,14 @@ $(document).ready(function() {
      $('body').delay(8100).append("<img src='patrick.png'> <form >  <input type='text'></form><button id='submit'>submit</button>");
         })
    $("body").on('click','#submit',function() {
-        alert('hi');
-
+        if ($('input:first').val() === 'yourmotherwasamurloc') {
+          alert('Correct, you may proceed.');
+          window.location.replace('secret.html');
+        } else {
+          alert('Wrong! Now get outta here!')
+          window.history.back()
+        }
+        
       });
  
      } )
