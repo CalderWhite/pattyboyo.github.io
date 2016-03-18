de48d82366a61905ba4f8995fd7cbebbc81e51f1
pattyboyo <patrick2hb@hotmail.com>
2016-03-18 15:12:55 -0400

    Update rps.js

--- game/rps.js
+++ game/rps.js
@@ -50,7 +50,25 @@ $(document).ready(function() {
 		} 
 	})
 	if (player1.choice > 0 && player2.choice > 0) {
-		battle();
+		function() {
+	if (player1.choice === player2.choice) {
+		win = 3;
+	} else if (player1.choice + 2 === player2.choice && player1.choice === 1){
+		win = 2;
+	} else if (player1.choice + 1 === player2.choice && player1.choice === 1) {
+		win = 1;
+	} else if (player1.choice + 1 === player2.choice && player1.choice === 2) {
+		win = 1;
+	} else if (player1.choice - 1 === player2.choice && player1.choice === 2) {
+		win = 2;
+	} else if (player1.choice - 1 === player2.choice && player1.choice === 3) {
+		win = 2;
+	} else if (player1.choice - 2 === player2.choice && player1.choice === 3) {
+		win = 1;
+	} else {
+		alert ('someone pressed the wrong button')
+	}
+}
 		if (win === 1) {
 			$('.winner').append('<p>player1 wins!</p>')
 		} else if (win === 2) {
