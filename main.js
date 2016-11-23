534a0049d21030170ab203f0faff45620714da71
pattyboyo <patrick2hb@hotmail.com>
2016-11-23 18:00:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
-  $('#title').fadeIn('1000');
+  
   window.setTimeout(function() {$('.pop:eq(0)').fadeIn('1000')},1000);
   window.setTimeout(function() {$('.pop:eq(1)').fadeIn('1000')},2000);
   window.setTimeout(function() {$('.pop:eq(2)').fadeIn('1000')},3000);
   window.setTimeout(function() {$('.pop:eq(3)').fadeIn('1000')},4000);
-
+  $('#title').fadeIn('1000');
   
   
   
