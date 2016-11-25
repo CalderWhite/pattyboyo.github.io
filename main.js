263786e2a679f7e0a6bcb389ba854e38326aae20
pattyboyo <patrick2hb@hotmail.com>
2016-11-24 19:39:07 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
   window.setTimeout(function() {$('.pop:eq(2)').fadeIn('1000')},3000);
   window.setTimeout(function() {$('.pop:eq(3)').fadeIn('1000')},4000);
   window.setTimeout(function() { $('#title').fadeIn('1000')},5000);
-  
+  $('.pop').hover(function(){
+    $(this).animate({height:250,width:250});
+  });
     
 } );
