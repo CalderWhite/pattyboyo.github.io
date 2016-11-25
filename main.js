54ca750b6572a99d9e4e03308d3db2adaf8ec0b7
pattyboyo <patrick2hb@hotmail.com>
2016-11-25 13:10:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
   
   window.setTimeout(function() {$('.pop:eq(0)').fadeIn('1000')},1000);
-  window.setTimeout(function() {$('.pop:eq(1)').fadeIn('1000')},2000);
-  window.setTimeout(function() {$('.pop:eq(2)').fadeIn('1000')},3000);
-  window.setTimeout(function() {$('.pop:eq(3)').fadeIn('1000')},4000);
-  window.setTimeout(function() { $('#title').fadeIn('1000')},5000);
+  window.setTimeout(function() {$('.pop:eq(1)').fadeIn('1000')},1500);
+  window.setTimeout(function() {$('.pop:eq(2)').fadeIn('1000')},2000);
+  window.setTimeout(function() {$('.pop:eq(3)').fadeIn('1000')},2500);
+  window.setTimeout(function() { $('#title').fadeIn('1000')},3500);
   $('.pop').hover(function(){
     $(this).filter(':not(:animated)').animate({height:250,width:250});
   }, function() {
